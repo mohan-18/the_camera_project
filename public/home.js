8a7a19b58a0c4ad3f9f0d124bdc1543e848b60ba
mohan-18 <mohangorai4@gmail.com>
2021-04-12 09:24:12 +0530

    Capture Feature included

--- public/home.js
+++ public/home.js
@@ -1,13 +1,17 @@
-var a = 6;
-console.log(a);
-var Start_Button = document.getElementById("Start_Button");
-var Stop_Button = document.getElementById("Stop_Button");
+const Start_Button = document.getElementById("Start_Button");
+const Stop_Button = document.getElementById("Stop_Button");
 const player = document.getElementById('player');
-Start_Button.addEventListener("click", function () {
-  const constraints = {
-    video: true,
-  };
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
 
+
+const constraints = {
+  video: true,
+};
+
+
+Start_Button.addEventListener("click", function () {
   navigator.mediaDevices.getUserMedia(constraints)
     .then((stream) => {
       player.srcObject = stream;
@@ -16,3 +20,26 @@ Start_Button.addEventListener("click", function () {
 Stop_Button.addEventListener("click", function () {
   player.srcObject.getVideoTracks().forEach(track => track.stop());
 });
+
+
+
+captureButton.addEventListener('click', () => {
+  // Draw the video frame to the canvas.
+  context.drawImage(player, 0, 0, canvas.width, canvas.height);
+
+  //The HTMLCanvasElement.toDataURL() method returns a data URI containing a representation
+  //of the image in the format specified by the type parameter (defaults to PNG).
+  var dataURL = canvas.toDataURL("image/jpeg", 1.0);
+
+  downloadImage(dataURL, 'my-canvas.jpeg');
+  
+});
+
+// Save | Download image
+function downloadImage(data, filename = 'untitled.jpeg') {
+  var a = document.createElement('a');
+  a.href = data;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+}
